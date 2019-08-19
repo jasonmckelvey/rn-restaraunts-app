a0a2243d4dbab1d68f92da1782b423b1e1d2eb2a
Jason <jason.mckelvey@companycam.com>
2019-08-19 08:50:08 -0500

    Updated code

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,22 +6,22 @@ import { Feather } from "@expo/vector-icons";
 const BarWrapper = styled.View`
   background-color: #e8edf1;
   border-radius: 6px;
-  padding: 8px 16px;
   flex-direction: row;
   align-items: center;
+  padding: 0px 8px;
 `;
 
-const Body = styled.Text`
+const Input = styled.TextInput`
   font-size: 16px;
-  margin-left: 8px;
   color: #7f91a8;
+  padding: 8px;
 `;
 
 const SearchBar = () => {
   return (
     <BarWrapper>
       <Feather name="search" size={24} color="#7F91A8" />
-      <Body>Search Bar</Body>
+      <Input>Search Bar</Input>
     </BarWrapper>
   );
 };
