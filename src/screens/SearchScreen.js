66bea819a1e83e7b86372cba0ef442aee7c06e36
Jason <jason.mckelvey@companycam.com>
2019-08-28 20:29:39 -0500

    Updated state on search results

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Text } from "react-native";
 import styled from "styled-components";
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const ScreenWrapper = styled.View`
   padding: 16px;
@@ -9,16 +10,24 @@ const ScreenWrapper = styled.View`
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  const searchApi = async () => {
+    const response = await yelp.get("/search", {
+      params: {
+        limit: 50,
+        term,
+        location: "san jose"
+      }
+    });
+    setResults(response.data.businesses);
+  };
 
   return (
     <ScreenWrapper>
-      <SearchBar
-        term={term}
-        onTermChange={newTerm => setTerm(newTerm)}
-        onTermSubmit={() => console.log("term was submitted")}
-      />
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
       <Text>Search Screen</Text>
-      <Text>{term}</Text>
+      <Text>We have found {results.length} results</Text>
     </ScreenWrapper>
   );
 };
